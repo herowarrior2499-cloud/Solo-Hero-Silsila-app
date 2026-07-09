8b9cd9b7bfcfce8d49ba22e35ce7e431049831ae
herowarrior2499-cloud <herowarrior2499@gmail.com>
2026-07-10 00:18:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const CACHE = 'chain-cache-v1';
+const CACHE = 'chain-cache-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+    caches.open(CACHE).then(async (cache) => {
+      // cache each file on its own so one failure doesn't wipe out the rest
+      await Promise.all(ASSETS.map((url) => cache.add(url).catch(() => {})));
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
   );
   self.clients.claim();
 });
@@ -19,7 +22,16 @@ self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
     caches.match(e.request).then((cached) => {
-      return cached || fetch(e.request).catch(() => cached);
+      const network = fetch(e.request)
+        .then((res) => {
+          if (res && res.ok && e.request.url.startsWith(self.location.origin)) {
+            const copy = res.clone();
+            caches.open(CACHE).then((cache) => cache.put(e.request, copy)).catch(() => {});
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
     })
   );
 });
